3df55fac13083affa3cd79f9075fec1ab9b60e68
rejohnbd <rejohn.cse@gmail.com>
2021-05-14 09:26:54 +0600

    Fix Cross Origin Resource Sharing

--- app.js
+++ app.js
@@ -4,6 +4,22 @@ const app = express();
 
 app.use(express.json());
 
+// For Support Cross Origin Request
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // '*' for any
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+    return res.status(200).json({});
+  }
+
+  next();
+});
+
 // Add Route
 const pharmacyRoute = require("./api/routes/Pharmacy");
 
